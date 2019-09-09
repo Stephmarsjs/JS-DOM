811b9fbcae5c45d2e125f47f9d13d2790e1371e3
Stephanie Rodriguez <stephanierodriguez.student@careerdevs.com>
2019-09-09 11:12:39 -0400

    innerHTML to get book-list

--- jsdom1.js
+++ jsdom1.js
@@ -1,13 +1,9 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
-
-
-var books = document.querySelector('#bok-list li .name');
-console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
+const books = document.quesrySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
-    console.log(book);
+    book.textContent += ' (Book titile)';
 });
+
+const bookList = document.querySeletor('#book-list');
+bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML content</p>';
